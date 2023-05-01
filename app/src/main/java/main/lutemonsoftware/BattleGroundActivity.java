e0d4d6d5e69f64115dca866cf5e2aad5ffa9ae7a
beingnator <aleksis.tammi@gmail.com>
2023-05-01 23:52:08 +0300

    BattleGroundin rakentamista

--- app/src/main/java/main/lutemonsoftware/BattleGroundActivity.java
+++ app/src/main/java/main/lutemonsoftware/BattleGroundActivity.java
@@ -1,7 +1,11 @@
 package main.lutemonsoftware;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +13,37 @@ import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BattleGroundActivity extends AppCompatActivity {
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_ground);
+
+
+        // If there isn't exactly 2 chosen Lutemons to fight, one must be returned to main activity. Waits 4 seconds before.
+        if ( Storage.getNumberOfSelectedLutemons() != 2 ) {
+            Toast.makeText(context, "Valitse listauksesta -KAKSI- Lutemonia taistellaksesi", Toast.LENGTH_LONG).show();
+            Thread closeActivity = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(4000);
+                        // Do some stuff
+                    } catch (Exception e) {
+                        e.getLocalizedMessage();
+                    }
+                }
+                });
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 
     public void startBattle(View view) {
@@ -26,8 +54,6 @@ public class BattleGroundActivity extends AppCompatActivity {
         CheckBox fighterFour = findViewById(R.id.checkFighter4);
         Button btnStartFight = findViewById(R.id.btnFight);
         TextView fightStats = findViewById(R.id.txtBattleStats);
-
-
 
 
     }
