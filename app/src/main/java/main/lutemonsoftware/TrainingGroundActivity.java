e60ec60b4e10ca070162d732f52feaf3a73bc86c
v9901 <markus.siivonen@gmail.com>
2023-05-02 21:02:50 +0300

    Treenaustoiminnallisuus, edit

--- app/src/main/java/main/lutemonsoftware/TrainingGroundActivity.java
+++ app/src/main/java/main/lutemonsoftware/TrainingGroundActivity.java
@@ -9,26 +9,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TrainingGroundActivity extends AppCompatActivity {
-    private Context context;
+    private Context context = this;
     private ArrayList<Lutemon> lutemons;
     private LutemonListAdapter adapter;
+    private Lutemon lutemon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_ground);
 
+        TextView lutemonName = findViewById(R.id.txtTrainName);
+        ImageView lutemonLogo = findViewById(R.id.imgTrainImg);
+
         lutemons = Storage.getInstance().getLutemons();
 
         adapter = new LutemonListAdapter(this, lutemons);
+
+        if ( Storage.getNumberOfSelectedLutemons() != 1 ) {
+            Toast.makeText(context, "Valitse listauksesta -YKSI- Lutemon, jota haluat treenata!", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            lutemon = Storage.getFirstSelectedLutemon();
+            lutemon = Storage.getFirstSelectedLutemon();
+            lutemonName.setText(lutemon.getName());
+            lutemonLogo.setImageResource(lutemon.getImage());
+        }
 
     }
 
@@ -37,7 +55,6 @@ public class TrainingGroundActivity extends AppCompatActivity {
 
         int attDefValue = -1;
         Lutemon lutemon = Storage.getInstance().getFirstSelectedLutemon();
-        context = TrainingGroundActivity.this;
 
         switch (rgAttDef.getCheckedRadioButtonId()) {
             case R.id.rbAttack:
@@ -52,7 +69,11 @@ public class TrainingGroundActivity extends AppCompatActivity {
                 break;
         }
 
-        adapter.notifyDataSetChanged();
+        if (attDefValue == -1) {
+            Toast.makeText(context, "Valitse hyökkäys tai puolustus ennen treenausta!", Toast.LENGTH_LONG).show();
+        } else {
+            adapter.notifyDataSetChanged();
+        }
 
     }
 
